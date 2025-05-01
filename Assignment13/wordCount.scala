14f2a19f3e4dfd2d55ca25bbd50fe4c28f026dca
Nikita Rajendra Londhe <146791442+Nikita25-londhe@users.noreply.github.com>
2025-05-01 06:56:56 +0530

    Create wordCount.scala

---
+++ Assignment13/wordCount.scala
@@ -0,0 +1,9 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+val session=SparkSession.builder.appName("myapp").master("local[*]").getOrCreate()
+import spark.implicits._
+val lines=spark.readStream.format("socket").option("host","localhost").option("port",9999).load()
+val words=lines.as[String].flatMap(_.split(" "))
+val count=words.groupBy("value").count()
+val query=count.writeStream.outputMode("complete").format("console").start()
+query.awaitTermination()
